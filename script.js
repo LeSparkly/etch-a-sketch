45eeb94d29009195bdaf6611fde4dd96db703097
LeSparkly <harley.ray2000@gmail.com>
2024-05-19 01:37:37 -0400

    fix recursion issue

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
+const gridSizeButton = document.querySelector(".size");
+const divContainer = document.createElement("div");
+divContainer.className = "container";
+document.body.appendChild(divContainer);
+
+gridSizeButton.addEventListener("click", () => {
+  getGridSize();
+});
+
+function getGridSize() {
+  let gridSize = prompt("Please enter a new grid size. LIMIT 100: ");
+  if (gridSize > 100) {
+    gridSize = 100;
+  }
+  console.log(`new grid size ${gridSize}`);
+  divContainer.innerHTML = "";
+
+  draw(gridSize);
+}
+
+function drawCol(gridSize) {
+  let cellSize = 256 / gridSize;
+  cellSize = cellSize + "px";
+  console.log(cellSize);
+
+  const drawColumn = document.createElement("div");
+  drawColumn.className = "column";
+  drawColumn.style.width = cellSize;
+  drawColumn.style.height = "256px";
+
+  for (let index = 0; index < gridSize; index++) {
+    const drawDiv = document.createElement("div");
+    drawDiv.style.width = cellSize;
+    drawDiv.style.height = cellSize;
+    drawDiv.className = "cell";
+    drawDiv.addEventListener("mouseover", () => {
+      drawDiv.className = "color";
+    });
+    drawColumn.appendChild(drawDiv);
+  }
+  divContainer.appendChild(drawColumn);
+}
+
 function draw(gridSize = 16) {
-  const gridSizeButton = document.querySelector(".size");
-
-  gridSizeButton.addEventListener("click", () => {
-    getGridSize();
-  });
-
-  function getGridSize() {
-    let gridSize = prompt("Please enter a new grid size. LIMIT 100: ");
-    if (gridSize > 100) {
-      gridSize = 100;
-    }
-    divContainer.remove();
-
-    draw(gridSize);
-  }
-
-  function drawCol(gridSize) {
-    let cellSize = 256 / gridSize;
-    cellSize = cellSize + "px";
-    console.log(cellSize);
-
-    const drawColumn = document.createElement("div");
-    drawColumn.className = "column";
-    drawColumn.style.width = cellSize;
-    drawColumn.style.height = "256px";
-
-    for (let index = 0; index < gridSize; index++) {
-      const drawDiv = document.createElement("div");
-      drawDiv.style.width = cellSize;
-      drawDiv.style.height = cellSize;
-      drawDiv.className = "cell";
-      drawDiv.addEventListener("mouseover", () => {
-        drawDiv.className = "color";
-      });
-      drawColumn.appendChild(drawDiv);
-    }
-    divContainer.appendChild(drawColumn);
-  }
-
-  const divContainer = document.createElement("div");
-  divContainer.className = "container";
-  document.body.appendChild(divContainer);
   for (let i = 0; i < gridSize; i++) {
     drawCol(gridSize);
   }
