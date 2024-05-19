e34497b0b80eb62d0883d0cc4e72b7d89d91434c
LeSparkly <harley.ray2000@gmail.com>
2024-05-19 01:21:25 -0400

    Allow user to set custom grid size

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-const divContainer = document.querySelector(".container");
+function draw(gridSize = 16) {
+  const gridSizeButton = document.querySelector(".size");
 
-function drawCol() {
-  const drawColumn = document.createElement("div");
-  drawColumn.className = "column";
-  drawColumn.style.width = "16px";
-  drawColumn.style.height = "256px";
+  gridSizeButton.addEventListener("click", () => {
+    getGridSize();
+  });
 
-  for (let index = 0; index <= 15; index++) {
-    const drawDiv = document.createElement("div");
-    drawDiv.style.width = "16px";
-    drawDiv.style.height = "16px";
-    drawDiv.className = "cell";
-    drawDiv.addEventListener("mouseover", () => {
-      drawDiv.className = "color";
-    });
-    drawColumn.appendChild(drawDiv);
+  function getGridSize() {
+    let gridSize = prompt("Please enter a new grid size. LIMIT 100: ");
+    if (gridSize > 100) {
+      gridSize = 100;
+    }
+    divContainer.remove();
+
+    draw(gridSize);
   }
-  divContainer.appendChild(drawColumn);
-  console.log("draw column");
-}
 
-function draw() {
-  for (let i = 0; i <= 15; i++) {
-    drawCol();
+  function drawCol(gridSize) {
+    let cellSize = 256 / gridSize;
+    cellSize = cellSize + "px";
+    console.log(cellSize);
+
+    const drawColumn = document.createElement("div");
+    drawColumn.className = "column";
+    drawColumn.style.width = cellSize;
+    drawColumn.style.height = "256px";
+
+    for (let index = 0; index < gridSize; index++) {
+      const drawDiv = document.createElement("div");
+      drawDiv.style.width = cellSize;
+      drawDiv.style.height = cellSize;
+      drawDiv.className = "cell";
+      drawDiv.addEventListener("mouseover", () => {
+        drawDiv.className = "color";
+      });
+      drawColumn.appendChild(drawDiv);
+    }
+    divContainer.appendChild(drawColumn);
+  }
+
+  const divContainer = document.createElement("div");
+  divContainer.className = "container";
+  document.body.appendChild(divContainer);
+  for (let i = 0; i < gridSize; i++) {
+    drawCol(gridSize);
   }
 }
 
